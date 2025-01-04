4a61ee59b058e9b9bbcc72949d0fce674d2187a0
bgmeulem <38312921+bgmeulem@users.noreply.github.com>
2025-01-04 14:45:14 +0000

    Deploying to gh-pages from @ bgmeulem/bgmeulem.github.io@e8f7d4607d52632eb17ff10d1ce9fb8e3f780db7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -474,13 +474,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/bjorge-meulemeester", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
